b43ea8d09bd6047dea6a2451a72293b67ee1b10c
Mina Ehab Khalil <minaehab161.me@gmail.com>
2021-12-03 18:45:58 +0200

    Cart module

--- app.js
+++ app.js
@@ -10,9 +10,77 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+
+//GET Requests
 app.get("/", function (req, res) {
   res.render("login");
 });
+
+app.get("/registration", function (req, res) {
+  res.render("registration");
+});
+
+app.get("/books", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("books");
+});
+
+app.get("/boxing", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("boxing");
+});
+
+app.get("/cart", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("cart", {cart: appUser.cart});
+});
+
+app.get("/galaxy", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("galaxy");
+});
+
+app.get("/home", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("home");
+});
+
+app.get("/iphone", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("iphone");
+});
+
+app.get("/leaves", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("leaves");
+});
+
+app.get("/phones", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("phones");
+});
+
+app.get("/searchresults", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("searchresults");
+});
+
+app.get("/sports", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("sports");
+});
+
+app.get("/sun", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("sun");
+});
+
+app.get("/tennis", function (req, res) {
+  if (appUser === null) res.render("login");
+  res.render("tennis");
+});
+
+//POST Requests
 
 app.post("/", async function (req, res) {
   var user = { username: req.body.username, password: req.body.password };
@@ -21,65 +89,37 @@ app.post("/", async function (req, res) {
   } else res.render("login");
 });
 
-app.get("/registration", function (req, res) {
-  res.render("registration");
-});
-
 app.post("/register", async function (req, res) {
   var user = { username: req.body.username, password: req.body.password };
   var created = await create(user);
   created ? res.render("home") : res.render("registration");
 });
 
-app.get("/books", function (req, res) {
-  res.render("books");
+app.post("/boxing", function (req, res) {
+  addToCart({name:"Boxing Bag", ref:"boxing"})
 });
 
-app.get("/boxing", function (req, res) {
-  res.render("boxing");
+app.post("/galaxy", function (req, res) {
+  addToCart({name:"Galaxy S21 Ultra", ref:"galaxy"})
 });
 
-app.get("/cart", function (req, res) {
-  res.render("cart");
+app.post("/iphone", function (req, res) {
+  addToCart({name:"iPhone 13 Pro", ref:"iphone"})
 });
 
-app.get("/galaxy", function (req, res) {
-  res.render("galaxy");
+app.post("/leaves", function (req, res) {
+  addToCart({name:"Leaves of Grass", ref:"leaves"})
 });
 
-app.get("/home", function (req, res) {
-  res.render("home");
+app.post("/sun", function (req, res) {
+  addToCart({name:"The Sun and Her Flowers", ref:"sun"})
 });
 
-app.get("/iphone", function (req, res) {
-  res.render("iphone");
+app.post("/tennis", function (req, res) {
+  addToCart({name:"Tennis Racket", ref:"tennis"})
 });
 
-app.get("/leaves", function (req, res) {
-  res.render("leaves");
-});
-
-app.get("/phones", function (req, res) {
-  res.render("phones");
-});
-
-app.get("/searchresults", function (req, res) {
-  res.render("searchresults");
-});
-
-app.get("/sports", function (req, res) {
-  res.render("sports");
-});
-
-app.get("/sun", function (req, res) {
-  res.render("sun");
-});
-
-app.get("/tennis", function (req, res) {
-  res.render("tennis");
-});
-
-//App user
+//App user and details
 var appUser = null;
 
 //Mongodb consts
@@ -105,8 +145,8 @@ async function create(user) {
     .collection("users")
     .findOne({ username: user.username });
   if (foundBefore === null) {
-    await client.db("projectdb").collection("users").insertOne(user);
-    appUser = user;
+    await client.db("projectdb").collection("users").insertOne({...user, cart: []});
+    appUser = await client.db("projectdb").collection("users").findOne(user);
   }
   client.close();
   return foundBefore === null ? true : false;
@@ -118,9 +158,20 @@ async function isUser(user) {
   var found = await client.db("projectdb").collection("users").findOne(user);
   client.close();
   if (found !== null) {
-    appUser = found._id;
+    appUser = found;
   }
   return found !== null ? true : false;
+}
+
+//Update Cart
+async function addToCart(item) {
+  await client.connect();
+  cart = appUser.cart;
+  cart.push(item);
+  await client.db("projectdb").collection("users").updateOne({_id: appUser._id}, { $set: {cart: cart } });
+  client.close();
+  appUser.cart = cart;
+  return;
 }
 
 app.listen(3000);
